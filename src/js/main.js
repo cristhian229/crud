bbbbb1e80e379ca59d1cc7c24118b7b71fa37322
cristhian229 <cristhianvasquez99@gmail.com>
2024-05-10 12:25:11 -0500

    eliminacion de la tabla

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,17 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
 import { coders } from "./database";
-import { create, index } from "./operations";
+import { create, index, coderToDelete } from "./operations";
 import {alertSmallSuccess} from "./alert";
 
-
+const table = document.querySelector("table")
 const tbody = document.querySelector("tbody");
-
-index(coders, tbody);
-
 const name = document.querySelector("#name");
 const lastName = document.querySelector("#last-name");
 const email = document.querySelector("#email");
 const form = document.querySelector("form");
+
+index(coders, tbody);
 
 form.addEventListener("submit", (event) => {
   
@@ -23,3 +22,22 @@ form.addEventListener("submit", (event) => {
   form.reset()
   event.preventDefault()
 });
+
+table.addEventListener("click", (event) =>{
+  if (event.target.classList.contains("btn-danger")) {
+    const toDelete = event.target.getAttribute("data-id")
+    console.log(toDelete)
+    coderToDelete(toDelete,coders)
+
+
+    alertSmallSuccess("coder eliminado")
+    index(coders,tbody)
+  }else{
+    alertSmallSuccess("le diste click a la tabla")
+  }
+  
+  
+  
+
+  
+})
--- src/js/operations.js
+++ src/js/operations.js
@@ -1,7 +1,5 @@
-
-
-export function index(array,tbody) {
-  tbody.innerHTML = ""
+export function index(array, tbody) {
+  tbody.innerHTML = "";
   array.forEach((coder) => {
     tbody.innerHTML += ` 
         <tr>
@@ -9,16 +7,29 @@ export function index(array,tbody) {
             <td>${coder.name}</td>
             <td>${coder.lastName}</td>
             <td class="text-lowercase">${coder.email}</td>
-        </tr>`
+            <td class="text-center align-middle">
+              <button type="button" data-id=${coder.id} class="btn btn-primary me-2">Details</button>
+              <button type="button" data-id=${coder.id} class="btn btn-warning mx-2">Edit</button>
+              <button type="button" data-id=${coder.id} class="btn btn-danger ms-2">Delete</button>
+            </td>
+        </tr>`;
   });
 }
 
- export function create(name,lastName,email,coders){
+export function create(name, lastName, email, coders) {
   const newCoder = {
     id: Date.now(),
     name: name.value,
     lastName: lastName.value,
-    email: email.value
-  }
-  coders.push(newCoder)
+    email: email.value,
+  };
+  coders.push(newCoder);
+}
+
+export function coderToDelete(idToDelete, coders){
+  coders.forEach((coder, index) => {
+    if (coder.id == idToDelete) {
+      coders.splice(index, 1)
+    }
+  })
 }
